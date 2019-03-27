584760949cb154433f50eba0eeada8ec3ae57b66
besobeso84 <34303252+besobeso84@users.noreply.github.com>
2019-03-27 13:14:27 +0400

    Lecture 3
    Delegate, Delegate Example

---
+++ ClassWork2/ClassWork2/changeString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassWork2
+{
+    class changeString
+    {
+        public static string changeSpaceToEmpty(string text) { 
+            return text.Replace(" ", String.Empty);
+        }
+    }
+}
